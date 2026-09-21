48e3c292c028e447bd89465b27012dda42da3491
Potatoyz908 <euller2005@gmail.com>
2026-09-21 19:22:56 -0300

    fix(main): remove importacao de processo_controller inexistente

--- main.py
+++ main.py
@@ -17,14 +17,13 @@ from starlette.types import ExceptionHandler  # noqa: E402
 
 from app.config.database import Base, SessionLocal, engine  # noqa: E402
 from app.config.limiter import limiter  # noqa: E402
-
 from app.controllers import (  # noqa: E402
     auth,
     cargo,
     funcionario,
     institucional,
     lancamento,
-    processo_controller, processo
+    processo,
 )
 from app.models.institucional import Institucional  # noqa: E402
 
@@ -72,7 +71,6 @@ app.include_router(cargo.router)
 app.include_router(funcionario.router)
 app.include_router(institucional.router)
 app.include_router(lancamento.router)
-app.include_router(processo_controller.router)
 app.include_router(processo.router)
 
 
